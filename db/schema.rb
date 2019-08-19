8978a3a5174c951617a06cd50046018348b269af
olesya ogorodnikova <ooleszja@gmail.com>
2019-08-19 16:52:50 +0200

    deleted extra migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_08_19_130558) do
+ActiveRecord::Schema.define(version: 2019_08_19_130842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
