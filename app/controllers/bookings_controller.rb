b7c940641dee6e00e19cddd563c45d6a3462f23a
gauthier HUBERT <gauthier.hubert@ehl.ch>
2019-08-20 13:01:04 +0200

    partial update booking controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,28 @@
+class BookingsController < ApplicationController
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new
+    end
+  end
+
+  def booking_params
+    params.require(:booking).permit(:date, :status)
+  end
+end
