0bea0dcdb4834b82d8be4491faef9c0156908edf
Christophe Bensmaine <christophe.bensmaine@laposte.net>
2019-08-20 17:04:12 +0200

    validates and select changes new announces

--- app/models/announce.rb
+++ app/models/announce.rb
@@ -2,8 +2,8 @@ class Announce < ApplicationRecord
   belongs_to :user
   mount_uploader :photo, PhotoUploader
   STATE = ["Comme neuf", "Excellent", "Bon état", "Fonctionnel"]
-  PRICES = ["1€","2€","3€","4€","5€","6€","7€","8€","9€","10€"]
-  validates :model, uniqueness: true, presence: true
+  PRICES = [1,2,3,4,5,6,7,8,9,10]
+  validates :model, presence: true
   validates :brand, presence: true
   validates :state, presence: true, inclusion: { in: STATE }
   validates :description, presence: true
