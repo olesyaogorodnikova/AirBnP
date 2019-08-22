03899bbd2af3f087324dfb9b61736be7230391cf
quentin deleglise <deleglise.quentin@hotmail.fr>
2019-08-22 16:34:23 +0200

    AddMapAndFixBug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,6 @@ loadDynamicBannerText();
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 initUpdateNavbarOnScroll();
 
-$(function () {
- $('[data-toggle="tooltip"]').tooltip()
-})
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
-
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
 
